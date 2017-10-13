ffd5278caed3ce2f31c58693ff1799dc916273ce
Moaz Khan <moaz.khan@teamo.io>
2017-10-13 10:24:27 +0500

    Added View controller for second tab
    Added View controller for second tab

---
+++ SwipeableTabBarController-ReplicatedIssue/SecondTabVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondTabVC.swift
+//  SwipeableTabBarController-ReplicatedIssue
+//
+//  Created by Moaz Khan on 10/13/17.
+//  Copyright © 2017 Teamo. All rights reserved.
+//
+
+import UIKit
+
+class SecondTabVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
