1e6922183b156712988ff04cd2b15aafd66f7f12
Moaz Khan <moaz.khan@teamo.io>
2017-10-13 09:58:32 +0500

    Initial Commit
    Initial Commit

---
+++ SwipeableTabBarController-ReplicatedIssue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeableTabBarController-ReplicatedIssue
+//
+//  Created by Moaz Khan on 10/13/17.
+//  Copyright © 2017 Teamo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
